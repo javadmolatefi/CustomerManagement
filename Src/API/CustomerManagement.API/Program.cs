30c06b98dfadb70454d87b68c2c55aa857911a75
Javad Molatefi <javadmolatefi@gmail.com>
2025-06-11 09:15:32 +0330

    add project references and basic dependency injection structure

--- Src/API/CustomerManagement.API/Program.cs
+++ Src/API/CustomerManagement.API/Program.cs
@@ -1,9 +1,13 @@
+using CustomerManagement.Application;
+using CustomerManagement.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ Src/Application/CustomerManagement.Application/ApplicationDependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomerManagement.Application;
+
+public static class ApplicationDependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        return services;
+    }
+}
